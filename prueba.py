78c666c1ab57d3c304dc56c252823710ff21f91a
tamandua01 <mbellesi01@gmail.com>
2020-08-20 20:09:20 -0300

    Primer Commit

---
+++ prueba.py
@@ -0,0 +1,11 @@
+numero = 5
+multiplicador = 10
+
+def multiplicar(a, b):
+    numero = 25
+    return a * b
+
+print(multiplicar(numero, multiplicador))
+print(numero)
+print(multiplicador)
+print(multiplicar(numero, multiplicador))
